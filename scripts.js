3d09d5fd341dcf887767cd608fd2ac1e8823cd7c
pydev <160148004+tusjsrrrftt@users.noreply.github.com>
2024-11-21 21:38:55 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Modal functionality
+var modal = document.getElementById("myModal");
+var btn = document.getElementsByClassName("open-modal")[0];
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
